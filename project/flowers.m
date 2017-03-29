dff50ec9cae9456693a24c392017b8b392bc41ad
Dekel Matalon <dekel2003@gmail.com>
2017-03-29 14:01:36 +0300

    complete picture frames solution

--- project/flowers.m
+++ project/flowers.m
@@ -2,19 +2,25 @@ clear;
 close all;
 
 %% load pictures to cell array
-img = cell(5,1);
-for k = 1:5
+num_frames = 17;
+img = cell(num_frames,1);
+for k = 1:num_frames
     fileName = sprintf('f%d.jpg', k);
     img{k} = imread(fileName);
 end
 
 %% measure distance between pics
 D = zeros(numel(img));
-for k1 = 1:4
-    for k2 = k1+1:5
-        img1 = rgb2gray(imresize(img{k1}, 0.4));
-        img2 = rgb2gray(imresize(img{k2}, 0.4));
+for k1 = 1:num_frames-1
+    for k2 = k1+1:num_frames
+%         img1 = rgb2gray(imresize(img{k1}, 0.4));
+%         img2 = rgb2gray(imresize(img{k2}, 0.4));
 
+        img1 = imresize(img{k1},0.4);
+        img1 = [img1(:,:,1) img1(:,:,2) img1(:,:,3)];
+        img2 = imresize(img{k2},0.4);
+        img2 = [img2(:,:,1) img2(:,:,2) img2(:,:,3)];
+        
 %         [optimizer, metric] = imregconfig('multimodal');
 %         optimizer.InitialRadius = 0.009;
 %         optimizer.Epsilon = 2;
@@ -55,8 +61,8 @@ scatter3(mds_dist(:,1),mds_dist(:,2),mds_dist(:,3));
 CC = [];
 PP = [];
 r = max(max(D))/pi;
-for k = 1:5
-    im = imresize(img{k}, 0.1);
+for k = 1:num_frames
+    im = imresize(img{k}, 0.3);
     z = mds_dist(k,:);
     p = size(im)/2;
     [H,W,~] = size(im);
@@ -75,13 +81,26 @@ for k = 1:5
 %         c = flipud(c);
 %     end
     c = reshape(c,[],3);
+    
+    % rotation
+    %     R = rotx(z*[0 1 0]'*180/pi);%* roty(-z*[1 0 0]'*180/pi);
+%     pos = normr(pos * R);
+    if z(3) > 0
+        R = rotx(z*[0 1 0]'*180/pi);
+        R = R * roty(z*[-1 0 0]'*180/pi);
+        pos(:,1:3) = pos(:,1:3) * R;
+    else
+        R = rotx(z*[0 1 0]'*180/pi);
+        R = R *roty(z*[-1 0 0]'*180/pi);
+        pos(:,1:3) = pos(:,1:3) * R';
+    end
 
     % translate
     T = [eye(3) z' ; 0 0 0 1];
     pos = pos * T';
-    pos = pos(:,1:3);
-    R = rotx(z*[0 1 0]') * roty(z*[1 0 0]');
-    pos = normr(pos * R);
+%     pos = pos(:,1:3);
+    pos = normr(pos(:,1:3));
+    
     PP=cat(1,PP,pos);
     CC=cat(1,CC,c);
 end
